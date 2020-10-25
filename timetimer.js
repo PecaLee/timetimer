f0a6dc0c97d97aa0e4c8b72191b9ce4c75537907
Peca Lee <studiopeca.lee@gmail.com>
2020-10-26 00:12:31 +0900

    range slider value print

---
+++ timetimer.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#form"),
+  slider = form.querySelector("#slider"),
+  sliderValue = form.querySelector("#sliderValue");
+
+function getValue() {
+  const v = slider.value;
+  return v;
+}
+
+function paintValue() {
+  const v = getValue();
+  sliderValue.innerHTML = v;
+}
+
+function init() {
+  paintValue();
+  slider.addEventListener("mousemove", paintValue);
+}
+
+init();
