461d220f7ff75dd48ca3e1cb24f3b5f3b43a748b
Peca Lee <studiopeca.lee@gmail.com>
2020-12-02 19:25:55 +0900

    macintel

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ let NOW;
 function isMobile() {
   let isMobile = false;
 
-  const filter = "win16|win32|win64|mac";
+  const filter = "win16|win32|win64|mac|macintel";
 
   if (navigator.platform) {
     isMobile = filter.indexOf(navigator.platform.toLowerCase()) < 0;
